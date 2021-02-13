b1571ba29f4bda16b5382b3940fac07a012b2001
Bogdan Zarioiu <bogdanzarioiu@gmail.com>
2021-02-13 22:11:55 +0100

    Implemented the basic UI containing some fake not todos and created the "Add new not to do" view.

--- NotToDo/ContentView.swift
+++ NotToDo/ContentView.swift
@@ -8,9 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var notTodos = Array(0..<20).map { _ in UUID().uuidString }
+    @State private var showNewNotToDo = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                VStack {
+                    Text("Enter here the things not to do!")
+                        .padding()
+                        .font(.system(size: 20, weight: .medium))
+                    List(notTodos, id: \.self) { todo in
+                        Text(todo)
+                        
+                    }
+                    .listStyle(PlainListStyle())
+                        
+                }
+                .navigationTitle("Not To Do")
+                
+                VStack {
+                    Spacer()
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            self.showNewNotToDo.toggle()
+                            
+                        }, label: {
+                            Image(systemName: "plus")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color(.systemPurple))
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        }).padding()
+                    }
+                }
+            }
+        }.sheet(isPresented: $showNewNotToDo, content: {
+            NewNotToDoView()
+        })
     }
 }
 
