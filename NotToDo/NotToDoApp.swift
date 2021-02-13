7fe7dbe906ebd57fcad886feb4e55077dc611adc
Bogdan Zarioiu <bogdanzarioiu@gmail.com>
2021-02-13 21:41:03 +0100

    Initial Commit

---
+++ NotToDo/NotToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotToDoApp.swift
+//  NotToDo
+//
+//  Created by Bogdan on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct NotToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
